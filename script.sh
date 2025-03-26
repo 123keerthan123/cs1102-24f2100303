b5d18e9fe9be1aeb3fba071ad0b227d619672f19
123keerthan123 <dkeerthan12@gmail.com>
2025-03-26 10:29:57 +0000

    Added script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export GITHUB_USERNAME="<your_github_username>"
-export GITHUB_REPOSITORY="cs1102-<roll_number>"
+export GITHUB_USERNAME="123keerthan123"
+export GITHUB_REPOSITORY="cs1102-24f2100303"
 
 echo "GitHub Username: $GITHUB_USERNAME"
 echo "GitHub Repository: $GITHUB_REPOSITORY"
